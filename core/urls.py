d313efaa0290a86bd5304ee2821ec6f03fc2479e
mrpal39 <RP9545416@GMAIL.COM>
2021-03-14 15:54:44 +0530

    first commit

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path,include
+from .import views
+urlpatterns = [
+    # path(),
+    # path(),
+    path('', views.home, name='index' ),
+    # path('', views.index, name='index' ),
+    # path('', views.index, name='index' ),
+
+]
+ 
